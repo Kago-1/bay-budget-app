2c7784fcf42e79ebd304cdff83333da20dfd3115
Steve <stephenkago10@gmail.com>
2022-10-07 09:28:30 +0300

    added  delete button

---
+++ src/components/Expense.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Expense({data,deleteExpense}) {
+  return (
+    <tr>
+    <td>{data.id}</td>
+    <td>{data.item}</td>
+    <td>{data.category}</td>
+    <td>{data.cost}</td>
+    <td><button className='btn-done' onClick={()=>deleteExpense(data.id)}>Mark done</button></td>
+    </tr>
+  )
+}
+
+export default Expense;
